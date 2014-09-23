f202b5c34e65fa3ff00ee4cca58399661216da1a
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2014-09-23 12:20:52 +0200

    Initial

---
+++ elasticpress.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: ElasticPress
+ * Plugin URI: http://www.3logic.it/elasticpress
+ * Description: Power wordpress search with Elasticsearch
+ * Version: 0.1.0
+ * Author: 3logic
+ * Author URI: http://www.3logic.it
+ * License: GPL2
+ */
+
